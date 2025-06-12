92d8c1583c31114c36f5ab7a96806fa170a9a1c6
Himanshu Chahal <himanshuchahal25@gmail.com>
2025-06-13 01:35:11 +0530

    feat: added get push update version function

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { getPushUpdateVersion } from 'react-native-app-push-update';
 
 export default function App() {
+  const version = async () => {
+    const v = await getPushUpdateVersion();
+    console.log(v);
+  };
+
+  useEffect(() => {
+    version();
+  }, []);
   return <View style={styles.container}></View>;
 }
 
@@ -9,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'yellow',
+    backgroundColor: 'red',
   },
 });
